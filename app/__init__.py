55390525ef1d08a5d06bbf8a24cf9862be799fb8
Shubham <shubhamranswal@gmail.com>
2025-11-15 13:23:08 +0530

    CI: make app importable in runner (setup.py + __init__.py); install package in CI

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-# app/__init__.py
 """
-TechGuru core package.
-This file makes 'app' importable as a package.
+TechGuru package root.
+
+This file makes `app` importable in CI and local dev.
 """
 __all__ = ["agent_core", "code_tools", "srs_scheduler", "scaffolder"]
